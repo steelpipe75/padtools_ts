18224fcf958c3ddc98a867250d4c9232c2d46068
N Y <steelpipe75@gmail.com>
2025-11-15 09:01:09 +0900

    chore(test): update golden file scripts

--- tests/cli/E2E/updateGoldenFile.sh
+++ tests/cli/E2E/updateGoldenFile.sh
@@ -3,6 +3,7 @@ rm ./tests/cli/E2E/output/sample_input_base_background_color_gray.svg.txt
 rm ./tests/cli/E2E/output/sample_input_base_background_color_none.svg.txt
 rm ./tests/cli/E2E/output/sample_input_base_background_color_white.svg.txt
 rm ./tests/cli/E2E/output/sample_input_font_size_20.svg.txt
+rm ./tests/cli/E2E/output/sample_input_list_render_type_TerminalOffset.svg.txt
 rm ./tests/cli/E2E/output/sample_input.svg.txt
 rm ./tests/cli/E2E/output_minified/sample_input.svg.txt
 
@@ -11,5 +12,6 @@ cp ./tests/cli/E2E/temp/sample_input_base_background_color_gray.svg ./tests/cli/
 cp ./tests/cli/E2E/temp/sample_input_base_background_color_none.svg ./tests/cli/E2E/output/sample_input_base_background_color_none.svg.txt
 cp ./tests/cli/E2E/temp/sample_input_base_background_color_white.svg ./tests/cli/E2E/output/sample_input_base_background_color_white.svg.txt
 cp ./tests/cli/E2E/temp/sample_input_font_size_20.svg ./tests/cli/E2E/output/sample_input_font_size_20.svg.txt
+cp ./tests/cli/E2E/temp/sample_input_list_render_type_TerminalOffset.svg ./tests/cli/E2E/output/sample_input_list_render_type_TerminalOffset.svg.txt
 cp ./tests/cli/E2E/temp/sample_input_with_--prettyprint_option.svg ./tests/cli/E2E/output/sample_input.svg.txt
 cp ./tests/cli/E2E/temp/sample_input_without_prettyprint_option.svg ./tests/cli/E2E/output_minified/sample_input.svg.txt
