59eb24909028101c6ed7ef5d074d9f0da1028742
N Y <steelpipe75@gmail.com>
2025-11-22 20:27:36 +0900

    npm run build

--- dist/cli/cli.js
+++ dist/cli/cli.js
@@ -119,10 +119,11 @@ commander_1.program
 commander_1.program
     .command("web")
     .description("Start a web server to serve the web application")
-    .action(() => {
+    .option("-p, --port <port>", "Port for the web server", (value) => parseInt(value, 10), 8080)
+    .action((options) => {
     var _a, _b;
     const webPath = path.join(__dirname, "..", "web");
-    const port = 8080;
+    const port = options.port;
     const command = `npx serve -s ${webPath} -l ${port}`;
     console.log(`Serving web application from: ${webPath}`);
     console.log(`Listening on http://localhost:${port}`);
