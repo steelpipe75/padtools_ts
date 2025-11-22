cfb7b51c25c0f9291fbf60fdac8c77a9ff548934
N Y <steelpipe75@gmail.com>
2025-11-22 23:32:07 +0900

    feat: Add Playwright E2E tests

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests/web',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], baseURL: 'http://localhost:1234' },
+    },
+  ],
+  webServer: {
+    command: 'npm run start:web',
+    url: 'http://localhost:1234',
+    reuseExistingServer: !process.env.CI,
+  },
+});
